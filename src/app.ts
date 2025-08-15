f258e4ff8cda88dedf9666ea748e5ceacccbb48e
rafi <rafiahmedrabby282@gmail.com>
2025-08-16 02:31:56 +0600

    added public folder

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,10 @@ app.use("/api/v1", router)
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({ message: "Welcome to Parcel Delivery system server " })
 })
-
+app.get("/loaderio-d1f49ca29e5889d6eee5dc8cf3b0e5c5/", (req, res) => {
+  res.type("text/plain");
+  res.send("loaderio-d1f49ca29e5889d6eee5dc8cf3b0e5c5");
+});
 app.use(globalErrorhandlers)
 
 app.use(notFound)
